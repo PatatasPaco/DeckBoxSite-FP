b6eca777cf306588393d1b62157a7d3c37f7fe57
Jose <jmmoyagonzalvez@gmail.com>
2025-12-11 19:40:11 +0100

    cards tabla

--- bbdd.sql
+++ bbdd.sql
@@ -16,3 +16,33 @@ CREATE TABLE IF NOT EXISTS usuarios (
     correo VARCHAR(100) UNIQUE NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS CARDS (
+    id VARCHAR(50) PRIMARY KEY, -- id del JSON
+    name VARCHAR(255),
+    type VARCHAR(100),
+    rarity VARCHAR(50),
+    domain TEXT, -- Array JSON
+    rune_cost INT,
+    power_cost INT,
+    might INT,
+    description TEXT,
+    flavor_text TEXT,
+    set_code VARCHAR(20),
+    card_number VARCHAR(20),
+    artist VARCHAR(255),
+    image_url TEXT,
+    tags TEXT, -- Array JSON
+    created_at TIMESTAMP,
+    market_price_usd DECIMAL(10, 2),
+    market_price_foil_usd DECIMAL(10, 2),
+    price_last_updated TIMESTAMP,
+    justtcg_id VARCHAR(255),
+    is_standard_only BOOLEAN,
+    card_back_id VARCHAR(50),
+    hide_from_compendium BOOLEAN,
+    is_promo BOOLEAN,
+    is_always_premium BOOLEAN,
+    has_proving_grounds_variant BOOLEAN,
+    copyright VARCHAR(100)
+);
+
